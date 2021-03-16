5c9b0be76dec3d9ed5528da8a324fa0f7412cdb6
DC <jessy.charlot@outlook.fr>
2021-03-16 02:55:43 +0100

    Display card for characters

--- app/containers/CharactersPage/CharactersTable.js
+++ app/containers/CharactersPage/CharactersTable.js
@@ -12,45 +12,36 @@ const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
+  tableCell: {
+    minWidth: 100,
+  },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function CharactersTable() {
+export default function CharactersTable(props) {
   const classes = useStyles();
-
+  const { characters } = props;
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          <TableRow className={classes.tableRow}>
+            <TableCell className={classes.tableCell}>ID</TableCell>
+            <TableCell className={classes.tableCell} align="right">Name</TableCell>
+            <TableCell className={classes.tableCell} align="right">Description</TableCell>
+            <TableCell className={classes.tableCell} align="right">Date</TableCell>
+            <TableCell className={classes.tableCell} align="right">Comics</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.name}>
+          {characters.map(character => (
+            <TableRow key={character.id}>
               <TableCell component="th" scope="row">
-                {row.name}
+                {character.id}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{character.name}</TableCell>
+              <TableCell align="right">{character.description}</TableCell>
+              <TableCell align="right">{character.modified}</TableCell>
+              <TableCell align="right">{character.comics.available}</TableCell>
             </TableRow>
           ))}
         </TableBody>
--- app/containers/CharactersPage/index.js
+++ app/containers/CharactersPage/index.js
@@ -7,19 +7,49 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { CheckBox } from '@material-ui/icons';
 import CharactersTable from './CharactersTable';
+import CharacterCard from './CharacterCard';
 import messages from './messages';
 
-// eslint-disable-next-line react/prefer-stateless-function
+const HEADERS = new Headers({
+  'Content-type': 'application/x-www-form-urlencoded',
+});
+
+const INIT = {
+  methode: 'GET',
+  headers: HEADERS,
+  mode: 'cors',
+};
+
 export default class CharactersPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchName: '',
+      searchName: 'a',
+      characters: [],
     };
+  }
+
+  componentDidMount() {
+    this.handleApiCall(this.state.searchName);
   }
 
   handleChange = e => {
     this.setState({ searchName: e.target.value });
+  };
+
+  handleApiCall = name => {
+    const API_URL = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${name}&apikey=3cb9f312871cec62f82dc980caeded2c`
+    fetch(API_URL, INIT)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ characters: json.data.results });
+      })
+      .catch(error => console.log(error)) //error json
+      .catch(error => console.log(error)) //error API
+  };
+
+  handleSearch = () => {
+    this.handleApiCall(this.state.searchName);
   };
 
   render() {
@@ -43,12 +73,21 @@ export default class CharactersPage extends React.Component {
             label="Search field"
             type="search"
           />
-          <Button variant="contained" color="inherit">
+          <Button
+            onClick={this.handleSearch}
+            variant="contained"
+            color="inherit"
+          >
             Search
           </Button>
           <CheckBox />
         </div>
-        <CharactersTable />
+        {this.state.characters[0] ? (
+          <CharacterCard character={this.state.characters[0]} />
+        ) : (
+          <div> </div>
+        )}
+        <CharactersTable characters={this.state.characters} />
       </div>
     );
   }
